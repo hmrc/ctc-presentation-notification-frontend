c3d08c8b71826acef842d4351b4b1a6e44e97514
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2023-10-03 11:49:54 +0100

    CTCP-3854: scalafmt

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -26,7 +26,7 @@ class Navigator {
 
   protected def normalRoutes(departureId: String, mode: Mode): PartialFunction[Page, UserAnswers => Option[Call]] = {
     case InferredLocationTypePage => ua => ???
-    case CoordinatesPage => ???
+    case CoordinatesPage          => ???
   }
 
   private def handleCall(userAnswers: UserAnswers, call: UserAnswers => Option[Call]) =
