4f23e37615a0521b1a4b0940633d3384598f541a
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-10-05 09:48:21 +0100

    Reformatted customs reference data config

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -31,7 +31,7 @@ class FrontendAppConfig @Inject() (config: Configuration, servicesConfig: MyServ
 
   val eccEnrolmentSplashPage: String = config.get[String]("urls.eccEnrolmentSplashPage")
 
-  lazy val referenceDataUrl: String = servicesConfig.fullServiceUrl("customsReferenceData")
+  lazy val referenceDataUrl: String = servicesConfig.fullServiceUrl("customs-reference-data")
 
   lazy val legacyEnrolmentKey: String           = config.get[String]("microservice.services.auth.legacy.enrolmentKey")
   lazy val legacyEnrolmentIdentifierKey: String = config.get[String]("microservice.services.auth.legacy.enrolmentIdentifierKey")
