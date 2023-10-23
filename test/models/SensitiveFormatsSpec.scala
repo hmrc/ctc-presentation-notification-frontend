7c9fb7b30daf4592946fc0c05323da869bd31cbc
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2023-10-23 17:01:22 +0100

    CTCP-4212: Simplifying message data value.

--- test/models/SensitiveFormatsSpec.scala
+++ test/models/SensitiveFormatsSpec.scala
@@ -16,9 +16,8 @@
 
 package models
 
-import base.TestMessageData.messageData
 import base.{AppWithDefaultMockFixtures, SpecBase}
-import models.messages.MessageData
+import models.messages.{Consignment, MessageData, TransitOperation}
 import play.api.libs.json.{JsObject, JsString, Json}
 import play.api.test.Helpers.running
 
@@ -26,6 +25,7 @@ class SensitiveFormatsSpec extends SpecBase with AppWithDefaultMockFixtures {
 
   "JsObject" - {
     val encryptedValue = "WFYrOuMf6WHDjHooyzED80QIGXMTPSHEjc3Kl8jPFRJFtHWV"
+
     val decryptedValue = Json.obj()
 
     "reads" - {
@@ -115,8 +115,21 @@ class SensitiveFormatsSpec extends SpecBase with AppWithDefaultMockFixtures {
 
   "MessageData" - {
     val encryptedValue =
-      "FIDE5xkbhsb3l8vaF7/aZysrk4RNDPzwII3gdHsXHoHS3LxwVo6Ivc0MId4ONyhia4a814xAlSeN2QBbsUf6gXlcaCm1aTm9xpE1l3lwA0ANxP44XHHKU/mF2CXGdiRWFDRiTjZH2aNFeYDnXSANsPYOemlRkZGNuDKG8O7xesUO6w5gPgsLEoYqvmvYTUW6Gm70GN9DaINyGBe/ItPHrSN7iUGRPj+Ckwlexh/1XHW+Rako+xejXHBO/dHu56S6Vs7x7XFYUXt9JbAdOGtXLBDORs6vWEvxy0JkYO+w5GmfYAtd1Mm0F/zMMNATRRnJJGxi6GC7m8aMYuRh25C0t78aq7vLha9ZXk0h4TgLEzQT3zsfypFrpw72iC97l/FhS3zLzScmTkTh+8Xt5TuDiT9Vlr/yC5ZQ/DEQu275a6piUBFfF/QLTT8FX9lnZiXj4GjO9gXaXKYren7NbQ+j5uHfYbjdtY2GlOJoPuRpQNH8smdYz3UARf3089FCZS0/FsM8QlBSJQt1gpZdKcs0GRMMwf4XtgHEMV1sCczhz7LbtT274Kv5CmzfSYhxIuxAjzsMDYODW/jxoF7pL5RP+uv5Gzc1oiBadINEKGxupNbITj42ZeCCztNQzbwICd68B0gEbDUN7F+MqV+E6Fi4eCw0X2zHXzx4KU+6My4t25upXDeyShe5Nq6Dlb5QrHlj3Re3p2/P1NBBtkYi6JeoeBHi/lRSCl4OSZJ8lcdgFUa+h8O/4nHIDSNARtzKD6roze7SOpxg3HHA3WeC8tAcTqnCSZSNcs8A6hkfj5NSP5ihcQUWWRi79xX6Rv4cys5xKnHNzRhBlJv7fOIBxHya1drirPaImuv1zqZeLXAoOt8d8U0kkjzjPcmWN/CTEcQCI8vAeKmTmk4topkNc3aRvtlv3TeNlFXs0h8UxWMpyW33AwzGhQbE96Jvllsf+mo+hwdEicbc3bfHJeOiJMF60fAqpfxXffdir2iovUMY4MvruH5e9Jwe55UDvDtixj18mDadzRQYsh9kSsokdOLaZAHyp/JzzmcDrmhvJZPNUt/dB5JAx7Kmzs0OazZuBSQM/eiuC2sCceyZa5H2bQhJEDMkEKgms6EaRqsIK4A28mW/pjAoq9fuK1U6FBDL0PZJOTtjJsreAa9f8r4iBzcv3CPXIFECLj4Hgk6AGLu6z+QKJMD1/lSxFel42ux0Lr2hDA7xwf/T/yukFrcA4Yzp4oxuW7ViHZMAwgEM/ZgVB9J+bYXWhP4bTt5hQ+j16gtzcO3cG/5AceY0d+cXQlQzFudgEjnCAfyprw7D+IPEp6qtPXrVgePT4WACC63jUqK2mVo3drAMG1ibvqjfWQDq7Zyse38be2N/6dPk1elK3B09XArGH5qSf4H6Op77y4iA/qvMgygsFVMVl9Rh4xnPRkpO9pDFyjhkFm9f/ov5+FN8cRBF+LXYxs55xcwBRuu91snZs9yHKjZRoB/O0vddDffRBH70Sgv06DMJRi04Vw8WNKSb7KVswW38Ax3/+3QiRTmwXCqDK+zafxe3zImu9DSYErg7Ls3ePv0xFtZiBz12+dRzo6c3GSGRHhy6blZvCozG8YM9eZDR8OeOodxS8KAccO0OCY7vWSB7A0cOaLC6j4Qr4xanzGVGUwGne+kHs8DsScjKbLrm7g+BjtTibmnp6IkJhmYXae01AQXzJPG1P/z7giacBP2PyMH0E9Qpq4Ch2utfIxrlMn31b5gLWKn/A7KHd1NhY1NuoeD1UKrHs32GBbLzGX4Q7i4KZeZBpeJXo2FnqWp9PgVTBEbA5yhB8pePGbKK9GOaKz3qLiau4ulwYUQZBGEOElnPQmjsvEjp5KUCHg2HzXA51fJxSeSxIGecTusGn99cj0KPHdQoahW/N0I8mncYcvRzWys9BdQrVUu/Tv6XTBYey7gDyBogJhPdaD+kbIVChZEYQJLEDGjeuQQXzKRvU+q7WZaRbdCr9WdorPimUfq8ymTlGV9pm8EswTHpoxAAv7VTO7pWC/JWjTtmhAHRRXzzvg/DoavNMnPp2ow6LUi7cXAvel8RKh5NfJWgXIoqG5/yE+owEzj3zbEyPoqqzZkMLNkO9S9IAel9eiY39cX2QkDc07QdtcfVUNFQQxjhN4CTjVaVIzJdHn4M7CZ+EIrb4+J7nSqdvnLWLgaiNPBsdUgB53g/oLxkFNBLV3Eu/3wIJVvdsuvcZVTudXbnHbIYU0ggvxW9mtfASmlYIetWUkt707E4URzTXjEN8mEsrqr62BkChuPH"
-    val decryptedValue = messageData
+      "iKyG8d8pfk2WgiyMzWSx1hTLnDM7wFoAzsXI/9KCGAIcn7IIIpI6MzHCdasbsRExN6wVZQCmW78jCeKjWrwPvSbWGmcv35IdDiMmMq3yyVw2VS+MaTIzdTK+l+9fC/gSlN+ttyOl4wGibGYhSr5m7N/sKeyvwrAttq9vzX9Ya//dMWrpbrQ="
+
+    val decryptedValue = MessageData(
+      CustomsOfficeOfDeparture = "",
+      TransitOperation = TransitOperation(None),
+      Authorisation = None,
+      Consignment = Consignment(
+        containerIndicator = None,
+        modeOfTransportAtTheBorder = None,
+        TransportEquipment = None,
+        LocationOfGoods = None,
+        ActiveBorderTransportMeans = None,
+        PlaceOfLoading = None
+      )
+    )
 
     "reads" - {
       "when encryption enabled" - {
