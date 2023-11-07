c28f1409689d734dbc291151389df2aaa8dc9b9a
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-11-07 14:23:45 +0000

    CTCP-4021: Add conveyance nav

--- app/navigation/BorderNavigator.scala
+++ app/navigation/BorderNavigator.scala
@@ -34,15 +34,18 @@ class BorderNavigator @Inject() () extends Navigator {
 
   override def normalRoutes(departureId: String, mode: Mode): PartialFunction[Page, UserAnswers => Option[Call]] = {
 
-    case BorderModeOfTransportPage             => ua => identificationPageNavigation(ua, departureId, mode)
-    case IdentificationPage(activeIndex)       => ua => IdentificationNumberPage(activeIndex).route(ua, departureId, mode)
-    case IdentificationNumberPage(activeIndex) => ua => NationalityPage(activeIndex).route(ua, departureId, mode)
-    case NationalityPage(activeIndex)          => ua => CustomsOfficeActiveBorderPage(activeIndex).route(ua, departureId, mode)
+    case BorderModeOfTransportPage                    => ua => borderModeNavigation(ua, departureId, mode)
+    case IdentificationPage(activeIndex)              => ua => IdentificationNumberPage(activeIndex).route(ua, departureId, mode)
+    case IdentificationNumberPage(activeIndex)        => ua => NationalityPage(activeIndex).route(ua, departureId, mode)
+    case NationalityPage(activeIndex)                 => ua => CustomsOfficeActiveBorderPage(activeIndex).route(ua, departureId, mode)
+    case CustomsOfficeActiveBorderPage(activeIndex)   => ua => customsOfficeNavigation(ua, departureId, mode, activeIndex)
+    case AddConveyanceReferenceYesNoPage(activeIndex) => ua => addConveyanceNavigation(ua, departureId,mode, activeIndex)
+    case ConveyanceReferenceNumberPage(activeIndex)   => ua => ???
   }
 
   override def checkRoutes(departureId: String, mode: Mode): PartialFunction[Page, UserAnswers => Option[Call]] = ???
 
-  private def identificationPageNavigation(ua: UserAnswers, departureId: String, mode: Mode): Option[Call] = {
+  private def borderModeNavigation(ua: UserAnswers, departureId: String, mode: Mode): Option[Call] = {
     val numberOfActiveBorderMeans: Int = ua.get(BorderActiveListSection).map(_.value.length).getOrElse(0)
 
     (ua.get(BorderModeOfTransportPage), ua.departureData.TransitOperation.security, ua.departureData.Consignment.ActiveBorderTransportMeans.isDefined) match {
@@ -52,5 +55,20 @@ class BorderNavigator @Inject() () extends Navigator {
       case _ => Some(routes.IdentificationController.onPageLoad(departureId, mode, Index(numberOfActiveBorderMeans)))
     }
   }
+
+  private def customsOfficeNavigation(ua: UserAnswers, departureId: String, mode: Mode, activeIndex: Index): Option[Call] = {
+    (ua.get(BorderModeOfTransportPage), ua.departureData.TransitOperation.security) match {
+      case (Some(BorderMode("4", "Air transport")), "1" || "2" || "3") =>
+        Some(routes.ConveyanceReferenceNumberController.onPageLoad(departureId, mode, activeIndex))
+      case _ => Some(routes.AddConveyanceReferenceYesNoController.onPageLoad(departureId, mode, activeIndex))
+    }
+  }
+
+  private def addConveyanceNavigation(userAnswers: UserAnswers, departureId: String, mode: Mode, activeIndex: Index): Option[Call] =
+    userAnswers.get(AddConveyanceReferenceYesNoPage(activeIndex)) match {
+      case Some(true) => ConveyanceReferenceNumberPage(activeIndex).route(userAnswers, departureId, mode)
+      case Some(false) => ???
+      case _ => Some(controllers.routes.SessionExpiredController.onPageLoad())
+    }
 
 }
