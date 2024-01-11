24bd9ad739bdc316e88edf268d57c75757caac4f
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2024-01-11 17:17:10 +0000

    CTCP-4306: Add view and page specs

---
+++ test/views/transport/departureTransportMeans/TransportMeansIdentificationNumberViewSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.transport.departureTransportMeans
+
+import forms.EnumerableFormProvider
+import forms.border.IdentificationNumberFormProvider
+import models.NormalMode
+import models.reference.transport.transportMeans.TransportMeansIdentification
+import org.scalacheck.{Arbitrary, Gen}
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
+import viewModels.InputSize
+import views.behaviours.{EnumerableViewBehaviours, InputTextViewBehaviours}
+import views.html.transport.departureTransportMeans.{TransportMeansIdentificationNumberView, TransportMeansIdentificationView}
+
+class TransportMeansIdentificationNumberViewSpec extends InputTextViewBehaviours[String] {
+
+  override def form: Form[String] = new IdentificationNumberFormProvider()(prefix)
+
+  val identificationType: TransportMeansIdentification = TransportMeansIdentification("80", "European vessel identification number (ENI Code)")
+
+  override def applyView(form: Form[String]): HtmlFormat.Appendable =
+    injector.instanceOf[TransportMeansIdentificationNumberView].apply(form, departureId, NormalMode, identificationType.asString)(fakeRequest, messages)
+
+  override val prefix: String = "consignment.departureTransportMeans.identificationNumber"
+
+  implicit override val arbitraryT: Arbitrary[String] = Arbitrary(Gen.alphaStr)
+
+  behave like pageWithTitle()
+
+  behave like pageWithBackLink()
+
+  behave like pageWithSectionCaption("Departure means of transport")
+
+  behave like pageWithHeading()
+
+  behave like pageWithInputText(Some(InputSize.Width20))
+
+  behave like pageWithHint("This can be up to 35 characters long and include both letters and numbers.")
+
+  behave like pageWithInsetText(identificationType.asString)
+
+  behave like pageWithSubmitButton("Continue")
+}
