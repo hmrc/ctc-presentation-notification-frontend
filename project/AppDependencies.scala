7576cd8058c08431478e04e00f0aef3de2484ccf
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-05-23 10:29:31 +0100

    CTCP-5218: Updated dependencies including play-frontend-hmrc v9.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,16 @@ object AppDependencies {
   private val hmrcMongoVersion = "1.6.0"
   private val catsVersion      = "2.9.0"
   private val monocleVersion   = "2.1.0"
-  private val pekkoVersion = "1.0.1"
+  private val pekkoVersion = "1.0.2"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "8.5.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "9.11.0",
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "com.chuusai"                %% "shapeless"                  % "2.3.10",
-    "org.typelevel"              %% "cats-core"                  % "2.9.0",
+    "com.chuusai"                %% "shapeless"                  % "2.3.12",
+    "org.typelevel"              %% "cats-core"                  % "2.10.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"        % "7.6.0",
-    "org.apache.commons"          % "commons-text"               % "1.10.0",
+    "org.apache.commons"          % "commons-text"               % "1.11.0",
     "com.github.julien-truffaut" %% "monocle-core"               % "2.1.0",
     "com.github.julien-truffaut" %% "monocle-macro"              % monocleVersion,
     "org.apache.pekko"           %% "pekko-actor"                % pekkoVersion,
@@ -24,14 +24,14 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"         %% "scalatest"               % "3.2.17",
+    "org.scalatest"         %% "scalatest"               % "3.2.18",
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.jsoup"              % "jsoup"                   % "1.15.4",
-    "org.mockito"            % "mockito-core"            % "5.2.0",
-    "org.scalatestplus"     %% "mockito-4-11"            % "3.2.17.0",
-    "org.scalatestplus"     %% "scalacheck-1-17"         % "3.2.17.0",
-    "org.scalacheck"        %% "scalacheck"              % "1.17.0",
+    "org.jsoup"              % "jsoup"                   % "1.17.2",
+    "org.mockito"            % "mockito-core"            % "5.11.0",
+    "org.scalatestplus"     %% "mockito-4-11"            % "3.2.18.0",
+    "org.scalatestplus"     %% "scalacheck-1-17"         % "3.2.18.0",
+    "org.scalacheck"        %% "scalacheck"              % "1.18.0",
     "io.github.wolfendale"  %% "scalacheck-gen-regexp"   % "1.1.0",
     "org.apache.pekko"      %% "pekko-testkit"          % pekkoVersion
   ).map(_ % "test")
