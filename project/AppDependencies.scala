4376fdf70ce0a9acef8cceaf14f69ee62797622e
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2024-10-16 16:49:51 +0100

    CTCP-5963: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.4.0"
-  private val hmrcMongoVersion = "2.1.0"
+  private val bootstrapVersion = "9.5.0"
+  private val hmrcMongoVersion = "2.2.0"
   private val catsVersion      = "2.9.0"
   private val monocleVersion   = "3.3.0"
   private val pekkoVersion = "1.0.3"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "10.3.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "10.13.0",
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"              %% "cats-core"                  % "2.12.0",
-    "uk.gov.hmrc"                %% "crypto-json-play-30"        % "8.0.0",
+    "uk.gov.hmrc"                %% "crypto-json-play-30"        % "8.1.0",
     "org.apache.commons"          % "commons-text"               % "1.11.0",
     "dev.optics"                 %% "monocle-core"               % monocleVersion,
     "dev.optics"                 %% "monocle-macro"              % monocleVersion,
