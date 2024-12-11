a2a3a6501e1b5a5c68a7315c4e1fde6a7503ba5a
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2024-12-11 15:28:51 +0000

    CTCP-6097: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "11.2.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "11.7.0",
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"              %% "cats-core"                  % "2.12.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"        % "8.1.0",
