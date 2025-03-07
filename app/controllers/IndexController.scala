2b3f9b88de57e44ee313c7edb97adf0500d3acb0
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-03-07 15:23:06 +0000

    Added logs to IndexController.

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -17,10 +17,11 @@
 package controllers
 
 import cats.data.OptionT
-import controllers.actions._
+import controllers.actions.*
 import generated.CC015CType
 import models.requests.OptionalDataRequest
 import models.{LocalReferenceNumber, RichCC015CType, UserAnswers}
+import play.api.Logging
 import play.api.i18n.I18nSupport
 import play.api.libs.json.JsObject
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -42,28 +43,38 @@ class IndexController @Inject() (
   dateTimeService: DateTimeService
 )(implicit ec: ExecutionContext)
     extends FrontendBaseController
-    with I18nSupport {
+    with I18nSupport
+    with Logging {
 
   def redirect(departureId: String): Action[AnyContent] = actions.getData(departureId).async {
     implicit request =>
-      (for {
-        lrn           <- OptionT.liftF(service.getLRN(departureId))
-        departureData <- OptionT(service.getDepartureData(departureId, lrn))
-        additionalDeclarationType = departureData.TransitOperation.additionalDeclarationType
-        canSubmitPresentationNotification <- OptionT.liftF(service.canSubmitPresentationNotification(departureId, lrn, additionalDeclarationType))
-        if canSubmitPresentationNotification
-        _ <- OptionT.liftF(
-          request.userAnswers match {
-            case Some(userAnswers) => sessionRepository.set(userAnswers)
-            case None              => generateFromDepartureData(departureId, lrn, departureData)
+      logger.info(s"Entering IE170 journey for departure ID $departureId")
+      (
+        for {
+          lrn <- OptionT.liftF(service.getLRN(departureId))
+          _ = logger.info(s"LRN $lrn found for departure ID $departureId")
+          departureData <- OptionT(service.getDepartureData(departureId, lrn))
+          _                         = logger.info(s"Departure data found for departure ID $departureId")
+          additionalDeclarationType = departureData.TransitOperation.additionalDeclarationType
+          canSubmitPresentationNotification <- OptionT.liftF(service.canSubmitPresentationNotification(departureId, lrn, additionalDeclarationType))
+          if canSubmitPresentationNotification
+          _ <- OptionT.liftF(
+            request.userAnswers match {
+              case Some(userAnswers) =>
+                logger.info(s"User answers found for departure ID $departureId")
+                sessionRepository.set(userAnswers)
+              case None =>
+                logger.info(s"No user answers found for departure ID $departureId")
+                generateFromDepartureData(departureId, lrn, departureData)
+            }
+          )
+        } yield
+          if (departureData.isDataComplete) {
+            Redirect(controllers.routes.CheckInformationController.onPageLoad(departureId))
+          } else {
+            Redirect(controllers.routes.MoreInformationController.onPageLoad(departureId))
           }
-        )
-      } yield
-        if (departureData.isDataComplete) {
-          Redirect(controllers.routes.CheckInformationController.onPageLoad(departureId))
-        } else {
-          Redirect(controllers.routes.MoreInformationController.onPageLoad(departureId))
-        }).getOrElse(Redirect(controllers.routes.ErrorController.technicalDifficulties()))
+      ).getOrElse(Redirect(controllers.routes.ErrorController.technicalDifficulties()))
   }
 
   private def generateFromDepartureData(
@@ -74,7 +85,8 @@ class IndexController @Inject() (
     val userAnswers = UserAnswers(departureId, request.eoriNumber, lrn.value, JsObject.empty, dateTimeService.currentInstant, departureData)
     for {
       updatedUserAnswers <- departureDataTransformer.transform(userAnswers)
-      result             <- sessionRepository.set(updatedUserAnswers)
+      _ = logger.info(s"Transformation complete for departure ID $departureId")
+      result <- sessionRepository.set(updatedUserAnswers)
     } yield result
   }
 }
