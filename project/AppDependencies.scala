8e79df331194498785885be7b6afb92e4157f753
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 14:39:31 +0100

    CTCP-6571: Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,16 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
-  private val catsVersion      = "2.9.0"
+  private val mongoVersion     = "2.6.0"
   private val monocleVersion   = "3.3.0"
-  private val pekkoVersion     = "1.0.3"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "12.6.0",
-    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % mongoVersion,
     "org.typelevel"              %% "cats-core"                  % "2.13.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"        % "8.2.0",
     "org.apache.commons"          % "commons-text"               % "1.13.1",
-    "dev.optics"                 %% "monocle-core"               % monocleVersion,
-    "dev.optics"                 %% "monocle-macro"              % monocleVersion,
-    "org.apache.pekko"           %% "pekko-actor"                % pekkoVersion,
     "javax.xml.bind"              % "jaxb-api"                   % "2.3.1",
     caffeine
   )
@@ -26,14 +21,15 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"         %% "scalatest"               % "3.2.19",
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % mongoVersion,
     "org.jsoup"              % "jsoup"                   % "1.21.1",
     "org.mockito"            % "mockito-core"            % "5.18.0",
     "org.scalatestplus"     %% "mockito-5-12"            % "3.2.19.0",
     "org.scalatestplus"     %% "scalacheck-1-18"         % "3.2.19.0",
     "org.scalacheck"        %% "scalacheck"              % "1.18.1",
     "io.github.wolfendale"  %% "scalacheck-gen-regexp"   % "1.1.0",
-    "org.apache.pekko"      %% "pekko-testkit"           % pekkoVersion
+    "dev.optics"            %% "monocle-core"            % monocleVersion,
+    "dev.optics"            %% "monocle-macro"           % monocleVersion
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
