c743152330fe586edcebd4dd1d1e7e09b1044b26
tudorsonycx <79589391+tudorsonycx@users.noreply.github.com>
2026-05-11 14:21:09 +0100

    CTCP6-249: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,33 +3,33 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
   private val mongoVersion     = "2.12.0"
   private val monocleVersion   = "3.3.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "12.31.0",
-    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % mongoVersion,
-    "org.typelevel"              %% "cats-core"                  % "2.13.0",
-    "uk.gov.hmrc"                %% "crypto-json-play-30"        % "8.4.0",
-    "org.apache.commons"          % "commons-text"               % "1.14.0",
-    "javax.xml.bind"              % "jaxb-api"                   % "2.3.1",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.5.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
+    "org.typelevel"     %% "cats-core"                  % "2.13.0",
+    "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0",
+    "org.apache.commons" % "commons-text"               % "1.15.0",
+    "javax.xml.bind"     % "jaxb-api"                   % "2.3.1",
     caffeine
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"         %% "scalatest"               % "3.2.19",
-    "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.jsoup"              % "jsoup"                   % "1.21.2",
-    "org.mockito"            % "mockito-core"            % "5.20.0",
-    "org.scalatestplus"     %% "mockito-5-12"            % "3.2.19.0",
-    "org.scalatestplus"     %% "scalacheck-1-18"         % "3.2.19.0",
-    "org.scalacheck"        %% "scalacheck"              % "1.19.0",
-    "io.github.wolfendale"  %% "scalacheck-gen-regexp"   % "1.1.0",
-    "dev.optics"            %% "monocle-core"            % monocleVersion,
-    "dev.optics"            %% "monocle-macro"           % monocleVersion
+    "org.scalatest"        %% "scalatest"               % "3.2.20",
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoVersion,
+    "org.jsoup"             % "jsoup"                   % "1.22.2",
+    "org.mockito"           % "mockito-core"            % "5.23.0",
+    "org.scalatestplus"    %% "mockito-5-12"            % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-18"         % "3.2.19.0",
+    "org.scalacheck"       %% "scalacheck"              % "1.19.0",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0",
+    "dev.optics"           %% "monocle-core"            % monocleVersion,
+    "dev.optics"           %% "monocle-macro"           % monocleVersion
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
